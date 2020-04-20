bdd0ab37cb93cbc8dce9c1d70c5776395767cead
Yves Piguet <yves.piguet@epfl.ch>
2020-04-20 11:52:52 +0200

    In demo_thymio.py, display of proximity sensor and exit upon button.center

--- demo_thymio.py
+++ demo_thymio.py
@@ -2,6 +2,7 @@
 # Author: Yves Piguet, EPFL
 
 import thymio.thymio
+import os
 
 if __name__ == "__main__":
 
@@ -22,15 +23,23 @@ if __name__ == "__main__":
     th[id]["leds.top"] = [0, 0, 32]
 
     # set a function called after new variable values have been fetched
+    prox_prev = 0
     def obs(node_id):
-        v = (th[node_id]["prox.horizontal"][5] - th[node_id]["prox.horizontal"][2]) // 10
-        th[node_id]["motor.left.target"] = v
-        th[node_id]["motor.right.target"] = v
-        if v > 5:
-            th[id]["leds.top"] = [0, 32, 0]
-        elif v < -5:
-            th[id]["leds.top"] = [32, 32, 0]
-        elif abs(v) < 3:
-            th[id]["leds.top"] = [0, 0, 32]
+        global prox_prev
+        prox = (th[node_id]["prox.horizontal"][5] - th[node_id]["prox.horizontal"][2]) // 10
+        if prox != prox_prev:
+            th[node_id]["motor.left.target"] = prox
+            th[node_id]["motor.right.target"] = prox
+            print(prox)
+            if prox > 5:
+                th[id]["leds.top"] = [0, 32, 0]
+            elif prox < -5:
+                th[id]["leds.top"] = [32, 32, 0]
+            elif abs(prox) < 3:
+                th[id]["leds.top"] = [0, 0, 32]
+            prox_prev = prox
+        if th[node_id]["button.center"]:
+            print("button.center")
+            os._exit(0) # forced exit despite coroutines
 
     th.set_variable_observer(id, obs)
